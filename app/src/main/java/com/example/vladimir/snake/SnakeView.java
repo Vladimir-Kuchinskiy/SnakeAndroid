a05395addc8e6d70413e014f9e70413f81cf5318
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-11-27 05:34:46 +0200

    adds helper methods for SnakeView class

--- app/src/main/java/com/example/vladimir/snake/SnakeView.java
+++ app/src/main/java/com/example/vladimir/snake/SnakeView.java
@@ -101,4 +101,89 @@ class SnakeView extends SurfaceView implements Runnable {
         // Start the game
         startGame();
     }
+
+    public void startGame() {
+        // Start with just a head, in the middle of the screen
+        m_SnakeLength = 1;
+        m_SnakeXs[0] = NUM_BLOCKS_WIDE / 2;
+        m_SnakeYs[0] = m_NumBlocksHigh / 2;
+
+        // And a mouse to eat
+        spawnMouse();
+
+        // Reset the m_Score
+        m_Score = 0;
+
+        // Setup m_NextFrameTime so an update is triggered immediately
+        m_NextFrameTime = System.currentTimeMillis();
+    }
+
+    public void spawnMouse() {
+        Random random = new Random();
+        m_MouseX = random.nextInt(NUM_BLOCKS_WIDE - 1) + 1;
+        m_MouseY = random.nextInt(m_NumBlocksHigh - 1) + 1;
+    }
+
+    private void eatMouse(){
+        //  Got one! Squeak!!
+        // Increase the size of the snake
+        m_SnakeLength++;
+        //replace the mouse
+        spawnMouse();
+        //add to the m_Score
+        m_Score = m_Score + 1;
+        m_SoundPool.play(m_get_mouse_sound, 1, 1, 0, 0, 1);
+    }
+
+    private void moveSnake(){
+        // Move the body
+        for (int i = m_SnakeLength; i > 0; i--) {
+            // Start at the back and move it
+            // to the position of the segment in front of it
+            m_SnakeXs[i] = m_SnakeXs[i - 1];
+            m_SnakeYs[i] = m_SnakeYs[i - 1];
+
+            // Exclude the head because
+            // the head has nothing in front of it
+        }
+
+        // Move the head in the appropriate m_Direction
+        switch (m_Direction) {
+            case UP:
+                m_SnakeYs[0]--;
+                break;
+
+            case RIGHT:
+                m_SnakeXs[0]++;
+                break;
+
+            case DOWN:
+                m_SnakeYs[0]++;
+                break;
+
+            case LEFT:
+                m_SnakeXs[0]--;
+                break;
+        }
+    }
+
+    private boolean detectDeath(){
+        // Has the snake died?
+        boolean dead = false;
+
+        // Hit a wall?
+        if (m_SnakeXs[0] == -1) dead = true;
+        if (m_SnakeXs[0] >= NUM_BLOCKS_WIDE) dead = true;
+        if (m_SnakeYs[0] == -1) dead = true;
+        if (m_SnakeYs[0] == m_NumBlocksHigh) dead = true;
+
+        // Eaten itself?
+        for (int i = m_SnakeLength - 1; i > 0; i--) {
+            if ((i > 4) && (m_SnakeXs[0] == m_SnakeXs[i]) && (m_SnakeYs[0] == m_SnakeYs[i])) {
+                dead = true;
+            }
+        }
+
+        return dead;
+    }
 }
